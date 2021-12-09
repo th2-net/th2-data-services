1435d0288e1baf214029cccdf691c93c87c130c6
GGregoryKK <kitaev200000@mail.ru>
2021-12-09 21:38:56 +0400

    TH2-2656: added test.

--- tests/tests_events_tree/test_events_tree_local.py
+++ tests/tests_events_tree/test_events_tree_local.py
@@ -25,8 +25,19 @@ def test_recover_events_tree(demo_data_source: DataSource):
     assert before_recover_unknown_events != after_recover_unknown_events and after_recover_unknown_events == 0
 
 
-def test_preserve_body_is_notset(demo_events_from_data_source: Data):
-    events_tree = EventsTree(demo_events_from_data_source)
+def test_preserve_body_is_notset(demo_data_source: DataSource):
+    start_time = datetime(year=2021, month=6, day=15, hour=9, minute=45, second=20, microsecond=692724)
+    end_time = datetime(year=2021, month=6, day=15, hour=12, minute=45, second=49, microsecond=28579)
+
+    events = demo_data_source.get_events_from_data_provider(
+        startTimestamp=start_time,
+        endTimestamp=end_time,
+        metadataOnly=False,
+    )
+
+    events_tree = EventsTree(events)
+    events_tree.recover_unknown_events(demo_data_source)
+
     with_body = set()
     for v in events_tree.events.values():
         if v.get("body") is not None:
@@ -37,8 +48,19 @@ def test_preserve_body_is_notset(demo_events_from_data_source: Data):
     assert False in with_body and len(with_body) is 1
 
 
-def test_preserve_body_is_false(demo_events_from_data_source: Data):
-    events_tree = EventsTree(demo_events_from_data_source, preserve_body=False)
+def test_preserve_body_is_false(demo_data_source: DataSource):
+    start_time = datetime(year=2021, month=6, day=15, hour=9, minute=45, second=20, microsecond=692724)
+    end_time = datetime(year=2021, month=6, day=15, hour=12, minute=45, second=49, microsecond=28579)
+
+    events = demo_data_source.get_events_from_data_provider(
+        startTimestamp=start_time,
+        endTimestamp=end_time,
+        metadataOnly=False,
+    )
+
+    events_tree = EventsTree(events, preserve_body=False)
+    events_tree.recover_unknown_events(demo_data_source)
+
     with_body = set()
     for v in events_tree.events.values():
         if v.get("body") is not None:
@@ -49,19 +71,7 @@ def test_preserve_body_is_false(demo_events_from_data_source: Data):
     assert False in with_body and len(with_body) is 1
 
 
-def test_preserve_body_is_true(demo_events_from_data_source: Data):
-
-    events_tree = EventsTree(demo_events_from_data_source, preserve_body=True)
-    with_body = set()
-    for v in events_tree.events.values():
-        if v.get("body") is not None:
-            with_body.add(True)
-        else:
-            with_body.add(False)
-    assert True in with_body and len(with_body) is 1
-
-
-def test_preserve_body_recover(demo_data_source: DataSource):
+def test_preserve_body_is_true_recover(demo_data_source: DataSource):
     start_time = datetime(year=2021, month=6, day=15, hour=9, minute=45, second=20, microsecond=692724)
     end_time = datetime(year=2021, month=6, day=15, hour=12, minute=45, second=49, microsecond=28579)
 
