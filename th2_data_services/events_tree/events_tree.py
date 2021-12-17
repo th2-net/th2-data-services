63407a976174d1f4faf3db62f159f11eace5f2c6
ConnectDIY <connect-soft@yandex.ru>
2021-12-18 01:17:18 +0300

    Fix th2-2794 issue and refactoring

--- th2_data_services/events_tree/events_tree.py
+++ th2_data_services/events_tree/events_tree.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from typing import Generator, Union, Iterator, Optional, Callable, Dict, List
 
@@ -27,7 +28,9 @@ class EventsTree:
 
     """
 
-    def __init__(self, data: Union[Iterator, Generator[dict, None, None], Data] = None, preserve_body: Optional[bool] = False):
+    def __init__(
+        self, data: Union[Iterator, Generator[dict, None, None], Data] = None, preserve_body: Optional[bool] = False
+    ):
         """
         Args:
             data: Events.
@@ -209,8 +212,22 @@ class EventsTree:
         return [e for e in self._events.values() if e["parentEventId"] == parent_event_id]
 
 
+class ITreeNodeShowFmt(ABC):
+    @abstractmethod
+    def __call__(self, pre, fill, node, **kwargs):
+        pass
+
+
 class TreeNode(Node):
     separator = " | "
+
+    class DefaultShowFmt(ITreeNodeShowFmt):
+        def __call__(self, pre, fill, node, **kwargs):
+            if kwargs["show_status"]:
+                status = "P" if node.data["successful"] else "F"
+                return "[%s] %s%s\n" % (status, pre, node.name)
+            else:
+                return "%s%s\n" % (pre, node.name)
 
     def __str__(self):
         s = ""
@@ -219,24 +236,19 @@ class TreeNode(Node):
             s += "[%s] %s%s\n" % (status, pre, node.name)
         return s
 
-    def show(self, fmt: Callable = None, failed_only=False, show_status=True):
-        if fmt is None:
+    @property
+    def full_path_name(self):
+        return self.separator.join([str(node.name) for node in self.path])
 
-            def fmt(pre, fill, node):
-                if show_status:
-                    status = "P" if node.data["successful"] else "F"
-                    return "[%s] %s%s\n" % (status, pre, node.name)
-                else:
-                    return "%s%s\n" % (pre, node.name)
-
+    def show(self, fmt: Callable = DefaultShowFmt, failed_only=False, show_status=True, **kwargs):
         s = ""
         if not failed_only:
             for pre, fill, node in RenderTree(self):
-                s += fmt(pre, fill, node)
+                s += fmt(pre, fill, node, show_status, **kwargs)
         else:
             for pre, fill, node in RenderTree(self):
                 if not node.data["successful"]:
-                    s += fmt(pre, fill, node)
+                    s += fmt(pre, fill, node, show_status, **kwargs)
         return s
 
     def get_by_status(self, status: bool):
@@ -249,15 +261,28 @@ class TreeNode(Node):
 class EventsTree2:
     """EventsTree2 - experimental tree."""
 
-    def __init__(self, data: Union[Iterator, Generator[dict, None, None], Data] = None, ds=None, preserve_body: Optional[bool] = False):
+    def __init__(
+        self,
+        data: Union[Iterator, Generator[dict, None, None], Data],
+        ds: DataSource,
+        preserve_body: bool = False,
+        broken_events: bool = False,
+        parentless: bool = False,
+    ):
         if data is None:
             data = []
         self.__preserve_body = preserve_body
+        self._broken_events = broken_events
+        self._parentless = parentless
         self._data_source = ds
         self._nodes = []
         self.roots: List[TreeNode] = []
         self.events_ids = []
         self.parent_events_ids = set()
+        self.__unknown_parent_ids = set()  # Set in order to remove duplicate Event IDs.
+        self.parentless_nodes = []  # TODO - нужнен параметр, который будет отвечать хотим ли мы такое
+        self._last_nodes_len = -1  # Just init value.
+        # Shows how many nodes were without a parent during the last iteration.
 
         self._build_tree(data)
 
@@ -282,8 +307,8 @@ class EventsTree2:
 
         unknown_parents_ids: list = self._get_unknown_parents_ids()
 
-        # restore them
-        restored_events = self._get_unknown_events(unknown_parents_ids)
+        # Restore them.
+        restored_events = self._get_unknown_events(unknown_parents_ids, broken_events=self._broken_events)
 
         for event in restored_events:
             event = event.copy()
@@ -293,51 +318,104 @@ class EventsTree2:
                 except KeyError:
                     pass
 
-            self._nodes.append(TreeNode(name=event["eventName"], data=event))
+            try:
+                self._nodes.append(TreeNode(name=event["eventName"], data=event))
+            except KeyError as er:
+                print(er)
+                print(event)
 
         # search roots
-        node: Node
+        node: TreeNode
         for node in self._nodes.copy():
-            if node.data["parentEventId"] is None:
+            if node.data["parentEventId"] is None:  # TODO - parentEventId
                 self.roots.append(node)
                 self._nodes.remove(node)
 
-        def x(roots):
-            if len(self._nodes) != 0:
+        try:
+            self._build_trees(self.roots)
+        except RecursionError:
+            pass  # TODO
+
+    def _build_trees(self, roots):
+        parentless_nodes_len = len(self._nodes)  # The number of nodes that are not included in any tree.
+        if parentless_nodes_len != 0:
+            if parentless_nodes_len != self._last_nodes_len:
+                self._last_nodes_len = parentless_nodes_len
                 new_roots = []
                 for node in self._nodes.copy():
                     for root_node in roots:
                         if node.data["parentEventId"] == root_node.data["eventId"]:
                             node.parent = root_node
-                            self._nodes.remove(node)
+                            try:
+                                self._nodes.remove(node)
+                            except ValueError as er:
+                                print(er)
+                                print("The Exception because Duplicate Nodes in the self._nodes!")
+                                print(node)
+                                print(self._nodes)
+                                raise
                             new_roots.append(node)
-                x(new_roots)
-            else:
-                return None
+                            break
+                self._build_trees(new_roots)
+            else:  # It happens if some events doesn't exist in DB and self._parentless == True
+                print(f"Cannot build complete EventsTree2, some nodes will be available as .parentless_nodes")
+                self.parentless_nodes = self._nodes
+                print(self.parentless_nodes)
+        else:
+            return None
 
-        x(self.roots)
-
-    def _get_unknown_events(self, unknown_parents, broken_events: Optional[bool] = False):
+    def _get_unknown_events(self, unknown_parents, broken_events) -> List[dict]:
+        """Recursion function to get all unknown events."""
         if unknown_parents:
-            new_events: list = self._data_source.find_events_by_id_from_data_provider(unknown_parents, broken_events)
+            # Populate set in order to remove duplicate Event IDs.
+            self.__unknown_parent_ids.update(set(unknown_parents))
 
-            unknown_parents2 = set()
+            if not broken_events:
+                new_events = []
+                for up_id in unknown_parents:
+                    try:
+                        new_events.append(self._data_source.find_events_by_id_from_data_provider(up_id, broken_events))
+                    except ValueError as err:
+                        owners = {[n for n in self._nodes if n.data["parentEventId"] == up_id]}
+                        if self._parentless:
+                            print(
+                                f"Cannot find in DB event with id: {up_id}\n"
+                                f"Owners: {owners}\n"
+                                f"EventsTree2 will be built with parentless_nodes"
+                            )
+                        else:
+                            print(err)
+                            print(f"Cannot build EventsTree2, due to some Nodes doesn't have parents")
+                            # Show Events that references to not founded Event.
+                            print(f"Owners: {owners}")
+                            raise
+            else:
+                new_events: list = self._data_source.find_events_by_id_from_data_provider(
+                    unknown_parents, broken_events
+                )
+
+            unknown_parents_for_next_recursion_step = set()
             new_events = [new_events] if not isinstance(new_events, list) else new_events
             for e in new_events:
-                parent_event_id = e["parentEventId"]
-                if parent_event_id is not None:
-                    unknown_parents2.add(parent_event_id)
+                parent_event_id = e["parentEventId"]  # TODO - parentEventId может иметь другое имя.
 
-            new_events += self._get_unknown_events(unknown_parents2)
+                if (
+                    parent_event_id is not None
+                    and parent_event_id != "Broken_Event"
+                    and parent_event_id not in self.__unknown_parent_ids
+                ):
+                    unknown_parents_for_next_recursion_step.add(parent_event_id)
+
+            new_events += self._get_unknown_events(unknown_parents_for_next_recursion_step, broken_events)
 
             return new_events
         else:
             return []
 
-    def _get_unknown_parents_ids(self) -> list:
-        """Searches unknown events.
+    def _get_unknown_parents_ids(self) -> List[str]:
+        """Searches unknown events ids.
 
-        :return: Unknown events.
+        :return: Unknown events ids.
         """
         unknown_parents_ids = []
 
