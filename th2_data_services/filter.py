24196c30e08456f0706544444ff615cf620f8d1c
GGregoryKK <kitaev200000@mail.ru>
2021-12-21 18:33:36 +0400

    [TH2-2878]: Added docstrings to filter class.

--- th2_data_services/filter.py
+++ th2_data_services/filter.py
@@ -1,5 +1,20 @@
+from typing import Union, Iterable
+
+
 class Filter:
-    def __init__(self, name: str, values: str or (list, tuple), negative: bool = False, conjunct: bool = False):
+    """The class that provides method of filters implementation URL of rpt-data-provider."""
+
+    def __init__(self, name: str, values: Union[Iterable, str], negative: bool = False, conjunct: bool = False):
+        """
+                Args:
+                    name (str): register the filter by its name.
+                    values (Union[Iterable, str]): One str with filter value or list of filter values.
+                    negative (bool):  If true, will match events/messages that do not match those specified values.
+                        If false, will match the events/messages by their values. Defaults to false.
+                    conjunct (bool): If true, each of the specific filter values should be applied
+                        If false, at least one of the specific filter values must be applied
+        """
+
         self.name = name
 
         if isinstance(values, (list, tuple)):
@@ -11,6 +26,14 @@ class Filter:
         self.conjunct = conjunct
 
     def url(self):
+        """Forms a filter.
+            For help use this readme:
+            https://github.com/th2-net/th2-rpt-data-provider#filters-api.
+
+                Returns:
+                    str: Formed filter.
+
+                """
         return (
             f"&filters={self.name}"
             + "".join([f"&{self.name}-values={val}" for val in self.values])
