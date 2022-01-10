dafe58f949496e90ae6a0499d147707a0462d753
Slava Ermakov <connect-soft@yandex.ru>
2022-01-10 12:19:02 +0300

    Update filter.py
    Update docstrings for Filter

--- th2_data_services/filter.py
+++ th2_data_services/filter.py
@@ -1,18 +1,18 @@
-from typing import Union, Iterable
+from typing import Union, List, Tuple
 
 
 class Filter:
-    """The class that provides method of filters implementation URL of rpt-data-provider."""
+    """The class for using rpt-data-provider filters API."""
 
-    def __init__(self, name: str, values: Union[Iterable, str], negative: bool = False, conjunct: bool = False):
+    def __init__(self, name: str, values: Union[List[str], Tuple[str], str], negative: bool = False, conjunct: bool = False):
         """
         Args:
-            name (str): register the filter by its name.
-            values (Union[Iterable, str]): One str with filter value or list of filter values.
+            name (str): Filter name.
+            values (Union[List[str], Tuple[str], str]): One string with filter value or list of filter values.
             negative (bool):  If true, will match events/messages that do not match those specified values.
                 If false, will match the events/messages by their values. Defaults to false.
             conjunct (bool): If true, each of the specific filter values should be applied
-                If false, at least one of the specific filter values must be applied
+                If false, at least one of the specific filter values must be applied.
         """
 
         self.name = name
