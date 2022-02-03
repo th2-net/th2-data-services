89df276e51b908828024a7967deae8188f1ab27c
Svyat935 <k.svyat395@gmail.com>
2022-02-03 16:54:10 +0300

    change resolver

--- th2_data_services/provider/v5/command_resolver.py
+++ th2_data_services/provider/v5/command_resolver.py
@@ -1,18 +1,16 @@
 from typing import Union
 
-from th2_data_services.provider.command import IProviderCommand
 from th2_data_services.provider.v5.commands.grpc import GetEventById as GetEventByIdFromGRPC
 from th2_data_services.provider.v5.commands.http import GetEventById as GetEventByIdFromHTTP
+
 from th2_data_services.provider.v5.data_source.grpc import GRPCProvider5DataSource
 from th2_data_services.provider.v5.data_source.http import HTTPProvider5DataSource
 
 
-class GetEventById(IProviderCommand):
-    def __init__(self, id: str):
-        self._id = id
-
-    def handle(self, data_source: Union[GRPCProvider5DataSource, HTTPProvider5DataSource]):
-        if isinstance(data_source, GRPCProvider5DataSource):
-            return GetEventByIdFromGRPC(self._id).handle(data_source)
-        else:
-            return GetEventByIdFromHTTP(self._id).handle(data_source)
+def resolver_get_event_by_id(data_source: Union[GRPCProvider5DataSource, HTTPProvider5DataSource]):
+    if isinstance(data_source, GRPCProvider5DataSource):
+        return GetEventByIdFromGRPC
+    elif isinstance(data_source, HTTPProvider5DataSource):
+        return GetEventByIdFromHTTP
+    else:
+        ValueError("Unknown DataSource Object.")
