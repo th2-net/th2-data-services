b88a78544d357f0cc62710d6c17a431e0fdeebad
Kotyakhov Svyatoslav <50461439+Svyat935@users.noreply.github.com>
2022-02-10 14:31:34 +0300

    [TH2-2970] Update events tree (#66)
    * Make grpc and http APIs look like each other
    
    + Added TODO comments for the team
    
    * correct api and existing commands
    
    * update commands and provider_api
    
    * little marks were corrected
    
    * fix considers
    
    * appened search basic checks and create request basic
    
    * add filters in build_basic_request_object
    
    * Fix commands and api for new grpc interface
    
    * finalize commands with adapters and stubs
    
    * update requirements.txt
    
    * changed string-fields on struct-fields
    
    * update events tree
    
    * refactoring code
    
    * added resolver
    
    * change resolver
    
    * added resolver
    
    * little fix
    
    * Change ET name to previous name
    
    Co-authored-by: ConnectDIY <connect-soft@yandex.ru>

--- th2_data_services/provider/v5/commands/http.py
+++ th2_data_services/provider/v5/commands/http.py
@@ -11,14 +11,23 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from typing import List
 
 from th2_data_services.provider.v5.command import IHTTPProvider5Command
 from th2_data_services.provider.v5.data_source.http import HTTPProvider5DataSource
 
 
 class GetEventById(IHTTPProvider5Command):
-    def __init__(self, id):
+    def __init__(self, id: str):
         self._id = id
+
+    def handle(self, data_source: HTTPProvider5DataSource):
+        pass
+
+
+class GetEventsById(IHTTPProvider5Command):
+    def __init__(self, ids: List[str]):
+        self._ids: ids
 
     def handle(self, data_source: HTTPProvider5DataSource):
         pass
--- th2_data_services/provider/v5/events_tree/events_tree.py
+++ th2_data_services/provider/v5/events_tree/events_tree.py
@@ -1,12 +1,173 @@
+from collections import defaultdict
+from typing import Callable, Generator, Iterator, Optional, Union
+
+from th2_data_services import Data
 from th2_data_services.et_interface import IEventsTree
+from th2_data_services.provider.v5.command_resolver import resolver_get_events_by_id
+from th2_data_services.provider.v5.data_source.grpc import GRPCProvider5DataSource
+from th2_data_services.provider.v5.data_source.http import HTTPProvider5DataSource
 from th2_data_services.provider.v5.struct import provider5_event_struct
 
 
-class EventsTree5(IEventsTree):
+class EventsTree(IEventsTree):
     def __init__(
         self,
         data: Union[Iterator, Generator[dict, None, None], Data] = None,
-        preserve_body: Optional[bool] = False,
+        data_source: Union[GRPCProvider5DataSource, HTTPProvider5DataSource] = None,
         event_struct=provider5_event_struct,
+        preserve_body: Optional[bool] = False,
     ):
-        """TBU by Sviatoslav"""
+        """
+        Args:
+            data: Events.
+            data_source: Data Source.
+            event_struct: Event Struct.
+            preserve_body ('bool', optional): If true keep events bodies.
+        """
+        if data is None:
+            data = []
+
+        self._preserve_body = preserve_body
+        self._data_source = data_source
+        self._event_struct = event_struct
+
+        self._events = {}  # {EventID_str: Event_dict}
+        self._unknown_events = defaultdict(lambda: 0)  # {parent_id: int(cnt)}
+        self.build_tree(data)
+
+    @property
+    def events(self) -> dict:
+        return self._events
+
+    @property
+    def unknown_events(self):
+        return self._unknown_events
+
+    def clear_events(self) -> None:
+        """Clear exist events."""
+        self._events = {}
+
+    def clear_unknown_events(self) -> None:
+        """Clear unknown events."""
+        self._unknown_events.clear()
+
+    def build_tree(self, data: Union[Iterator, Generator[dict, None, None]]) -> None:
+        """Build or append new events to family tree.
+
+        Args:
+            data: Events.
+        """
+        for event in data:
+            event = event.copy()
+            self.append_element(event)
+        self.search_unknown_parents()
+
+    def append_element(self, event: dict) -> None:
+        """Append new event to events tree.
+
+        Will update the event if event_id matches.
+        Will remove the event from unknown_events if it in unknown_events dict.
+
+        Args:
+            event: Event
+        """
+        event_id = event["eventId"]
+        if not self._preserve_body:
+            try:
+                event.pop("body")
+            except KeyError:
+                pass
+
+        self._events[event_id] = event
+
+        event_id = event["eventId"]
+        if event_id in self._unknown_events:
+            self._unknown_events.pop(event_id)
+
+    def search_unknown_parents(self) -> dict:
+        """Searches unknown events.
+
+        Returns:
+             dict: Unknown events.
+        """
+        self.clear_unknown_events()
+        event: dict
+        for event in self.events.values():
+            parent_id = event["parentEventId"]
+            if parent_id is not None:
+                if parent_id == "Broken_Event":
+                    continue
+                if parent_id not in self._events:
+                    parent_id = event["parentEventId"]
+                    self._unknown_events[parent_id] += 1
+
+        return self._unknown_events
+
+    def is_ancestor_by_condition(self, event: dict, check_function: Callable) -> bool:
+        """Checks that events ancestor passed condition.
+
+        Args:
+            event: Event.
+            check_function: Condition function.
+
+        Returns:
+            True if condition is passed.
+        """
+        parent_id = event.get("parentEventId")
+        if parent_id is None:
+            return False
+
+        ancestor = self._events.get(parent_id)
+        while ancestor:
+            if check_function(event):
+                return True
+            ancestor = self._events.get(ancestor.get("parentEventId"))
+        return False
+
+    def get_ancestor_by_condition(self, event: dict, condition_function: Callable) -> Optional[dict]:
+        """Gets event ancestor by condition.
+
+        Args:
+            event: Event.
+            condition_function: Condition function.
+
+        Returns:
+            Ancestor event If condition is True.
+        """
+        parent_id = event.get("parentEventId")
+        if parent_id is None:
+            return None
+
+        ancestor = self._events.get(parent_id)
+        while ancestor:
+            if condition_function(event):
+                return ancestor
+            ancestor = self._events.get(ancestor.get("parentEventId"))
+        return ancestor
+
+    def recover_unknown_events(self, stub: Optional[bool] = False) -> None:
+        """Loads unknown events from data provider and recover EventsTree.
+
+        Args:
+            stub: True If you need handle broken events.
+        """
+        command_get_events_by_id = resolver_get_events_by_id(self._data_source)
+
+        old_unknown_events = self._unknown_events.keys()
+        while self._unknown_events:
+            instance_command = command_get_events_by_id(self._unknown_events.keys())
+            if stub:
+                instance_command = command_get_events_by_id.use_stub()
+
+            new_events = self._data_source.command(instance_command)
+            if isinstance(new_events, dict):
+                new_events = [new_events]
+            if new_events is None:
+                new_events = []
+            self.build_tree(new_events)
+            if self._unknown_events == old_unknown_events:
+                break
+            old_unknown_events = self._unknown_events.copy()
+
+    def get_children(self, parent_event_id) -> list:
+        return [e for e in self._events.values() if e["parentEventId"] == parent_event_id]
