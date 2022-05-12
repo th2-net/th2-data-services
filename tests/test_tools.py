fd938da9302b019eced908071d75b383395ee31a
GGregoryKK <kitaev200000@mail.ru>
2022-05-12 17:26:43 +0400

    [TH2-3467]: Fix remarks.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -5,12 +5,19 @@ import pytest
 
 def test_get_time_obj_from_string():
     dt = datetime.fromisoformat("2022-03-05 23:56:44.233856")
+    dt_1 = datetime.fromisoformat("2022-03-05 23:56:44")
     assert all(
         [
             tools.get_time_obj_from_string("2022-03-05T23:56:44.233856Z", "datetime") == dt,
             int(dt.timestamp() * 10 ** 6) * 1000 == tools.get_time_obj_from_string("2022-03-05T23:56:44.233856Z"),
             int(dt.timestamp() * 10 ** 6)
             == tools.get_time_obj_from_string("2022-03-05T23:56:44.233856Z", "microseconds"),
+            tools.get_time_obj_from_string("2022-03-05T23:56:44.0Z", "datetime") == dt_1,
+            int(dt_1.timestamp() * 10 ** 6) * 1000 == tools.get_time_obj_from_string("2022-03-05T23:56:44.0Z"),
+            int(dt_1.timestamp() * 10 ** 6) == tools.get_time_obj_from_string("2022-03-05T23:56:44.0Z", "microseconds"),
+            tools.get_time_obj_from_string("2022-03-05T23:56:44Z", "datetime") == dt_1,
+            int(dt_1.timestamp() * 10 ** 6) * 1000 == tools.get_time_obj_from_string("2022-03-05T23:56:44Z"),
+            int(dt_1.timestamp() * 10 ** 6) == tools.get_time_obj_from_string("2022-03-05T23:56:44Z", "microseconds"),
         ]
     )
 
@@ -18,18 +25,27 @@ def test_get_time_obj_from_string():
         tools.get_time_obj_from_string("2022-03-05 23:56:44.233856", "datetime")
         tools.get_time_obj_from_string("2022-03-05 23:56:44.233856", "microseconds")
         tools.get_time_obj_from_string("2022-03-05 23:56:44.233856")
-        tools.get_time_obj_from_string("2022-03-05 23:56:44.233856", "datetime")
         tools.get_time_obj_from_string("2022-03-05T23:56:44.233856Z", "milliseconds")
 
 
 def test_get_time_obj_from_timestamp():
-    d = {"nano": 477000000, "epochSecond": 1634223323}
-    dt = datetime.fromtimestamp(1634223323.477000000)
+    d = {"nano": 1, "epochSecond": 1}
+    dt = datetime.fromtimestamp(1.000000001)
+    d_1 = {"nano": 0, "epochSecond": 0}
+    dt_1 = datetime.fromtimestamp(0.000000000)
+    d_2 = {"nano": 999999999, "epochSecond": 1}
+    dt_2 = datetime.fromtimestamp(1.999999999)
     assert all(
         [
             tools.get_time_obj_from_timestamp(d, "datetime") == dt,
-            tools.get_time_obj_from_timestamp(d, "microseconds") == int(dt.timestamp() * 10 ** 6),
-            tools.get_time_obj_from_timestamp(d) == int(dt.timestamp() * 10 ** 6) * 1000,
+            tools.get_time_obj_from_timestamp(d, "microseconds") == 1000000,
+            tools.get_time_obj_from_timestamp(d) == 1000000001,
+            tools.get_time_obj_from_timestamp(d_1, "datetime") == dt_1,
+            tools.get_time_obj_from_timestamp(d_1, "microseconds") == 0,
+            tools.get_time_obj_from_timestamp(d_1) == 0,
+            tools.get_time_obj_from_timestamp(d_2, "datetime") == dt_2,
+            tools.get_time_obj_from_timestamp(d_2, "microseconds") == 1999999,
+            tools.get_time_obj_from_timestamp(d_2) == 1999999999,
         ]
     )
     incorrect_d_0 = {"nano": 4770.0000, "epochSecond": 1634223323}
--- th2_data_services/tools/tools.py
+++ th2_data_services/tools/tools.py
@@ -1,20 +1,21 @@
 from datetime import datetime
 
 
-def get_time_obj_from_string(date_string: str, format: str = "nanoseconds") -> (datetime, int):
+def get_time_obj_from_string(datetime_string: str, format: str = "nanoseconds") -> (datetime, int):
     """Parse datetime object from a string to different format.
 
     Args:
-        date_string: Source datetime string to convert.
-        format: Transform parameter. Default value: 'nanoseconds'.
+        datetime_string: Source datetime string to convert.
+        format: Transform parameter.
+            Values: 'nanoseconds', 'microseconds', 'datetime'. Defaults to 'nanoseconds'.
 
     Returns:
         obj: Converted object.
     """
     try:
-        timestamp = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        timestamp = datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S.%fZ")
     except ValueError:
-        timestamp = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+        timestamp = datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%SZ")
     if format == "nanoseconds":
         return int(timestamp.timestamp() * 10 ** 6) * 1000
     elif format == "microseconds":
@@ -22,7 +23,7 @@ def get_time_obj_from_string(date_string: str, format: str = "nanoseconds") -> (
     elif format == "datetime":
         return timestamp
     else:
-        raise ValueError(f"Format does not match argument: '{format}'")
+        raise ValueError(f"Format does not support the value: '{format}'")
 
 
 def get_time_obj_from_timestamp(timestamp: dict, format: str = "nanoseconds") -> (datetime, int):
@@ -30,17 +31,18 @@ def get_time_obj_from_timestamp(timestamp: dict, format: str = "nanoseconds") ->
 
     Args:
         timestamp:  Th2 timestamp format.
-        format: Transform parameter. Default value: 'nanoseconds'.
+        format: Transform parameter.
+            Values: 'nanoseconds', 'microseconds', 'datetime'. Defaults to 'nanoseconds'.
 
     Returns:
         obj: Converted object.
     """
-    _ts = int(f"{timestamp['epochSecond']}{timestamp['nano']:0>9}")
+    datetime_string = f"{timestamp['epochSecond']}{timestamp['nano']:0>9}"
     if format == "nanoseconds":
-        return _ts
+        return int(datetime_string)
     elif format == "microseconds":
-        return _ts // 10 ** 3
+        return int(datetime_string[:-3])
     elif format == "datetime":
-        return datetime.fromtimestamp(_ts / 10 ** 9)
+        return datetime.fromtimestamp(float(f"{timestamp['epochSecond']}.{timestamp['nano']:0>9}"))
     else:
-        raise ValueError(f"Format does not match argument: '{format}'")
+        raise ValueError(f"Format does not support the value: '{format}'")
