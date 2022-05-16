65dff576307f074896936d15056b351d295e9033
Vyacheslav Yermakov <vyacheslav.yermakov@exactprosystems.com>
2022-05-16 10:46:08 +0300

    Improve tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,46 +1,63 @@
 from th2_data_services.tools import tools
 from datetime import datetime
 import pytest
+from collections import namedtuple
+
+TestCase = namedtuple("TestCase", ["datetime_string", "expected_datetime", "expected_ns", "expected_us"])
 
 
-@pytest.mark.parametrize(
-    "input_obj, expected",
-    [
-        (("2022-03-05T23:56:44.233856Z", "datetime"), datetime.fromisoformat("2022-03-05 23:56:44.233856")),
-        (
-            ("2022-03-05T23:56:44.233856Z", "nanoseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44.233856").timestamp() * 10 ** 9),
-        ),
-        (
-            ("2022-03-05T23:56:44.233856Z", "microseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44.233856").timestamp() * 10 ** 6),
-        ),
-        (("2022-03-05T23:56:44.0Z", "datetime"), datetime.fromisoformat("2022-03-05 23:56:44")),
-        (
-            ("2022-03-05T23:56:44.0Z", "microseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44").timestamp() * 10 ** 6),
-        ),
-        (
-            ("2022-03-05T23:56:44.0Z", "nanoseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44").timestamp() * 10 ** 9),
-        ),
-        (("2022-03-05T23:56:44Z", "datetime"), datetime.fromisoformat("2022-03-05 23:56:44")),
-        (
-            ("2022-03-05T23:56:44Z", "microseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44").timestamp() * 10 ** 6),
-        ),
-        (
-            ("2022-03-05T23:56:44Z", "nanoseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44").timestamp() * 10 ** 9),
-        ),
-        (
-            ("2022-03-05T23:56:44.000000001Z", "nanoseconds"),
-            int(datetime.fromisoformat("2022-03-05 23:56:44").timestamp() * 10 ** 9) + 1,
-        ),
-    ],
-)
-def test_get_time_obj_from_string(input_obj, expected):
-    assert tools.get_time_obj_from_string(*input_obj) == expected
+@pytest.fixture(params=["nanoseconds", "ns"])
+def format_ns(request):
+    return request.param
+
+
+@pytest.fixture(params=["microseconds", "us"])
+def format_us(request):
+    return request.param
+
+
+# For 2022-03-05T23:56:44
+nanoseconds = 1646513804_000_000_000
+microseconds = 1646513804_000_000
+
+
+@pytest.fixture(params=[
+    TestCase("2022-03-05T23:56:44.0Z",
+             expected_datetime=datetime(year=2022, month=3, day=5, hour=23, minute=56, second=44),
+             expected_ns=nanoseconds,
+             expected_us=microseconds),
+    TestCase("2022-03-05T23:56:44Z",
+             expected_datetime=datetime(year=2022, month=3, day=5, hour=23, minute=56, second=44),
+             expected_ns=nanoseconds,
+             expected_us=microseconds),
+    TestCase("2022-03-05T23:56:44.123456Z",
+             expected_datetime=datetime(year=2022, month=3, day=5, hour=23, minute=56, second=44, microsecond=123456),
+             expected_ns=nanoseconds + 123_456_000,
+             expected_us=microseconds + 123_456),
+    TestCase("2022-03-05T23:56:44.123456789Z",
+             expected_datetime=datetime(year=2022, month=3, day=5, hour=23, minute=56, second=44, microsecond=123456),
+             expected_ns=nanoseconds + 123_456_789,
+             expected_us=microseconds + 123_456),
+    TestCase("2022-03-05T23:56:44.123Z",
+             expected_datetime=datetime(year=2022, month=3, day=5, hour=23, minute=56, second=44, microsecond=123),
+             expected_ns=nanoseconds + 123_000_000,
+             expected_us=microseconds + 123_000),
+])
+def datetime_strings(request) -> TestCase:
+    return request.param
+
+
+def test_get_time_obj_from_string_ns(datetime_strings, format_ns):
+    assert tools.get_time_obj_from_string(datetime_strings.datetime_string, format_ns) == datetime_strings.expected_ns
+
+
+def test_get_time_obj_from_string_us(datetime_strings, format_us):
+    assert tools.get_time_obj_from_string(datetime_strings.datetime_string, format_us) == datetime_strings.expected_us
+
+
+def test_get_time_obj_from_string_datatime(datetime_strings):
+    assert tools.get_time_obj_from_string(datetime_strings.datetime_string,
+                                          "datetime") == datetime_strings.expected_datetime
 
 
 @pytest.mark.parametrize(
