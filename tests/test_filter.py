8b599210990121c1a769744d53610b2dc30c4b0e
Slava Ermakov <connect-soft@yandex.ru>
2022-07-20 11:14:13 +0300

    Remove redundant copyright

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -2,18 +2,17 @@ from th2_data_services.filter import Filter
 
 
 def test_filter_url():
-    assert all(
-        [
-            Filter("type", ["one", 2, "three"], False, False).url()[1:]
-            == "filters=type&type-values=one&type-values=2&type-values=three&type-negative=False",
-            Filter("type", ["one", 2, "three"], True, True).url()[1:]
-            == "filters=type&type-values=one&type-values=2&type-values=three&type-negative=True",
-            Filter("type", ["one", 2, "three"], False, True).url()[1:]
-            == "filters=type&type-values=one&type-values=2&type-values=three&type-negative=False",
-            Filter("type", ["one", 2, "three"], True, False).url()[1:]
-            == "filters=type&type-values=one&type-values=2&type-values=three&type-negative=True",
-        ]
+    filter_ = Filter("type", ["one", 2, "three"], False, False)
+    assert (
+        filter_.url()
+        == "&filters=type&type-values=one&type-values=2&type-values=three&type-negative=False&type-conjunct=False"
     )
+
+    filter_ = Filter("name", "one", False, False)
+    assert filter_.url() == "&filters=name&name-values=one&name-negative=False&name-conjunct=False"
+
+    filter_ = Filter("name", 1)
+    assert filter_.url() == "&filters=name&name-values=1&name-negative=False&name-conjunct=False"
 
 
 def test_filter_grcp():
