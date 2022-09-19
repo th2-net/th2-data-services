241274bfd94f8b8b0d8bcdb1889704566f4c92a6
xHacka <pvpgamerpro457@gmail.com>
2022-09-19 14:16:57 +0000

    [TH2-4222] Updated CI Tests

--- tests/test_dependency_install.py
+++ tests/test_dependency_install.py
@@ -13,15 +13,20 @@ lwdp = "th2-data-services-lwdp"
 
 if provider:
     if provider == 'rdp':
+        print(f"Installing --> {provider=}")
         import th2_data_services_rdp
     elif provider == 'rdp5':
+        print(f"Installing --> {provider=}")
         import th2_data_services_rdp
         assert packages[rdp][0] == '5'
     elif provider == 'rdp6':
+        print(f"Installing --> {provider=}")
         import th2_data_services_rdp
         assert packages[rdp][0] == '6'
     elif provider == 'lwdp':
+        print(f"Installing --> {provider=}")
         import th2_data_services_lwdp
     elif provider == 'lwdp1':
+        print(f"Installing --> {provider=}")
         import th2_data_services_lwdp
         assert packages[lwdp][0] == '1'
