219da7bb9112ab27ba0897b67c38127e0d642880
xHacka <pvpgamerpro457@gmail.com>
2022-09-19 11:35:12 +0000

    [TH2-4122] Updated CI Tests

---
+++ tests/test_dependency_install.py
@@ -0,0 +1,27 @@
+from sys import argv
+from pip._internal.operations.freeze import freeze
+
+packages = {}
+for pkg in freeze():
+    k, v = pkg.split("==")
+    packages[k] = v
+
+provider = argv[1] if len(argv) > 1 else None
+
+rdp = "th2-data-services-rdp"
+lwdp = "th2-data-services-lwdp"
+
+if provider:
+    if provider == 'rdp':
+        import th2_data_services_rdp
+    elif provider == 'rdp5':
+        import th2_data_services_rdp
+        assert packages[rdp][0] == '5'
+    elif provider == 'rdp6':
+        import th2_data_services_rdp
+        assert packages[rdp][0] == '6'
+    elif provider == 'lwdp':
+        import th2_data_services_rdp
+    elif provider == 'lwdp1':
+        import th2_data_services_rdp
+        assert packages[lwdp][0] == '1'
