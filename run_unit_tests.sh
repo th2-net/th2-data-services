bf901c07423369571bafd4708d75cd9bbe0c087c
Slava Ermakov <connect-soft@yandex.ru>
2022-11-03 12:31:53 +0530

    Try new req versions and win CI

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -12,6 +12,8 @@ pytest tests/tests_unit -n auto --ignore tests/${PATH_DIFF_VERSION}/tests_v5
 # RDP 5
 echo '----========RDP5 tests========----'
 pip install th2-grpc-data-provider==0.1.6 -U -q
+pip install th2-grpc-common 3.4.0
+pip install protobuf==3.20.3
 pip install mypy-protobuf==2.5
 #pin install th2-grpc-common==3.11.1
 pip list
