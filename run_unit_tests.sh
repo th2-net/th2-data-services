f58f49f8ce37d41ec03b7f3b7397613c5a590271
Slava Ermakov <connect-soft@yandex.ru>
2022-11-03 11:52:34 +0530

    Try new req versions and win CI

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -3,14 +3,14 @@ PATH_DIFF_VERSION="tests_unit/tests_diff_version"
 
 # RDP 6
 echo '----========RDP6 tests========----'
-pip list
 pip install th2-grpc-data-provider==1.1.0 -U -q
+pip list
 pytest tests/tests_unit -n auto --ignore tests/${PATH_DIFF_VERSION}/tests_v5
 
 
 # RDP 5
 echo '----========RDP5 tests========----'
-pip list
 pip install th2-grpc-data-provider==0.1.6 -U -q
+pip list
 pytest tests/tests_unit -n auto --ignore tests/${PATH_DIFF_VERSION}/tests_v6
 
