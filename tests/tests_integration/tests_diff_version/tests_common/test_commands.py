04c3b7b4ff5a2a2c2ec9f2737508aa45d356feba
turikotura <turikotura@outlook.com>
2022-11-23 08:45:01 +0000

    Updated iteration break test to new fixture

--- tests/tests_integration/tests_diff_version/tests_common/test_commands.py
+++ tests/tests_integration/tests_diff_version/tests_common/test_commands.py
@@ -1,29 +1,31 @@
 from th2_data_services import Data
 
-def test_break_iteration_get_events(demo_events_from_data_source: Data):
+def test_break_iteration_get_events(all_events):
+    data = all_events.data
     iterated_one_item = False
-    for _ in demo_events_from_data_source:
+    for _ in data:
         if iterated_one_item:
             break
         iterated_one_item = True
 
     count = 0
 
-    for _ in demo_events_from_data_source:
+    for _ in data:
         count += 1
 
-    assert count == 49 # Length of demo_events_from_data_source
-
-def test_break_iteration_get_messages(demo_messages_from_data_source: Data):
+    assert count ==  len(all_events.expected_data_values)
+1
+def test_break_iteration_get_messages(all_messages):
+    data = all_messages.data
     iterated_one_item = False
-    for _ in demo_messages_from_data_source:
+    for _ in data:
         if iterated_one_item:
             break
         iterated_one_item = True
 
     count = 0
 
-    for _ in demo_messages_from_data_source:
+    for _ in data:
         count += 1
 
-    assert count == 239 # Length of demo_messages_from_data_source
+    assert count == len(all_messages.expected_data_values)
