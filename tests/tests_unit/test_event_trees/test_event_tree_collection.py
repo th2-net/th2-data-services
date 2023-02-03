2d8c9e58fd00d1d7138ad7852ec16d3a57066a0d
xHacka <pvpgamerpro457@gmail.com>
2023-02-03 13:30:39 +0000

    Tests moved

--- tests/tests_unit/test_event_trees/test_event_tree_collection.py
+++ tests/tests_unit/test_event_trees/test_event_tree_collection.py
@@ -291,3 +291,18 @@ def test_all_after_get_parentless_trees(demo_etc, dummy_etc_data):
     test_etc_find_event(demo_etc, True)
     test_etc_find_events(demo_etc, True)
     test_etc_find_ancestor(demo_etc, True)
+
+
+def test_findall_max_count(random_ETC):
+    etc = random_ETC
+    max_nodes_to_get = 10
+    findall_nodes = etc.findall(filter=lambda e: e.get("parentEventId"), max_count=max_nodes_to_get)
+    assert len(findall_nodes) == max_nodes_to_get
+
+
+def test_findall_iter_max_count(random_ETC):
+    etc = random_ETC
+    one_value_from_findall = list(etc.findall_iter(filter=lambda e: e.get("parentEventId") is not None, max_count=1))
+    assert [
+        {"eventName": "Event D0", "eventId": "D0_id", "data": {"data": [63, 40, 40]}, "parentEventId": "root_id28"}
+    ] == one_value_from_findall
