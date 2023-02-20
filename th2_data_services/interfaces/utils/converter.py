69b389ab4c09da8161f416f5414277e2d04f19a4
turikotura <turikotura@outlook.com>
2023-02-20 15:31:29 +0400

    Updated converter interface

--- th2_data_services/interfaces/utils/converter.py
+++ th2_data_services/interfaces/utils/converter.py
@@ -1,3 +1,16 @@
+#  Copyright 2022-2023 Exactpro (Exactpro Systems Limited)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 from abc import ABC, abstractmethod
 from datetime import datetime, timezone
 from typing import Generic, TypeVar
@@ -28,7 +41,7 @@ class ITimestampConverter(ABC, Generic[TimestampType]):
         """
         seconds, nanoseconds = cls.parse_timestamp(timestamp)
         microseconds = nanoseconds[:-3]
-        return datetime.utcfromtimestamp(seconds).replace(tzinfo=timezone.utc, microsecond=int(microseconds))
+        return datetime.utcfromtimestamp(int(seconds)).replace(tzinfo=timezone.utc, microsecond=int(microseconds))
 
     @classmethod
     def to_microseconds(cls, timestamp: TimestampType) -> int:
@@ -44,6 +57,21 @@ class ITimestampConverter(ABC, Generic[TimestampType]):
         """
         seconds, nanoseconds = cls.parse_timestamp(timestamp)
         return int(f"{seconds}{nanoseconds[:-3]}")
+
+    @classmethod
+    def to_milliseconds(cls, timestamp: TimestampType):
+        """Converts timestamp to milliseconds.
+
+        If your timestamp has nanoseconds, they will be just cut (not rounding).
+
+        Args:
+            timestamp: TimestampType object to convert.
+
+        Returns:
+            int: Timestamp in microseconds format.
+        """
+        seconds, nanoseconds = cls.parse_timestamp(timestamp)
+        return int(f"{seconds}{nanoseconds[:-6]}")
 
     @classmethod
     def to_nanoseconds(cls, timestamp: TimestampType) -> int:
