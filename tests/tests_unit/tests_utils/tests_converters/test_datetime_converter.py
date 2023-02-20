bc16a91775f5e9a19bfcc468b4eaf7678b2ebe35
Turikotura <68775241+Turikotura@users.noreply.github.com>
2023-02-20 15:37:56 +0400

    Backported converters from dev2 (#234)
    * Backported converters from dev2
    
    * Fix copyright
    
    * Added tests
    
    ---------
    
    Co-authored-by: Slava Ermakov <connect-soft@yandex.ru>

---
+++ tests/tests_unit/tests_utils/tests_converters/test_datetime_converter.py
@@ -0,0 +1,18 @@
+from th2_data_services.utils.converters import DatetimeConverter
+
+
+def test_to_datetime(datetime_strings):
+    assert datetime_strings.expected_datetime == DatetimeConverter.to_datetime(datetime_strings.datetime_obj)
+
+
+def test_to_microseconds(datetime_strings):
+    assert datetime_strings.expected_us == DatetimeConverter.to_microseconds(datetime_strings.datetime_obj)
+
+
+def test_to_nanoseconds(datetime_strings):
+    """If you request nanoseconds, last 3 number will be zeros, because datatime object doesn't have nanoseconds."""
+    assert datetime_strings.expected_us * 1000 == DatetimeConverter.to_nanoseconds(datetime_strings.datetime_obj)
+
+
+def test_to_milliseconds(datetime_strings):
+    assert datetime_strings.expected_ms == DatetimeConverter.to_milliseconds(datetime_strings.datetime_obj)
