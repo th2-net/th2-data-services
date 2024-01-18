f523fc5fcbcb2099507b7cfd960ace869aa0793a
turikotura <turikotura@outlook.com>
2024-01-18 17:50:37 +0400

    Added tests for unix timestamp converter

---
+++ tests/tests_unit/tests_utils/tests_converters/test_unix_timestamp_converter.py
@@ -0,0 +1,36 @@
+from th2_data_services.utils.converters import UnixTimestampConverter
+
+
+def test_to_datetime(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_datetime(datetime_strings.expected_ns)
+        == datetime_strings.expected_datetime
+    )
+
+
+def test_to_microseconds(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_microseconds(datetime_strings.expected_ns)
+        == datetime_strings.expected_us
+    )
+
+
+def test_to_nanoseconds(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_nanoseconds(datetime_strings.expected_ns)
+        == datetime_strings.expected_ns
+    )
+
+
+def test_to_milliseconds(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_milliseconds(datetime_strings.expected_ns)
+        == datetime_strings.expected_ms
+    )
+
+
+def test_to_datetime_string(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_datetime_str(datetime_strings.expected_ns)
+        == datetime_strings.expected_datestring
+    )
