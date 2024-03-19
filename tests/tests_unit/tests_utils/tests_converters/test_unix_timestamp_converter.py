512c1403e9e77c7c5285d987d47473bfcf46b90c
davitmamrikishvili <86967699+davitmamrikishvili@users.noreply.github.com>
2024-03-19 18:27:53 +0400

    th2-5176 - Add to_th2_timestamp (#286)
    * Add to_th2_timestamp
    
    * Add docstring
    
    * Update pydocstyle version

--- tests/tests_unit/tests_utils/tests_converters/test_unix_timestamp_converter.py
+++ tests/tests_unit/tests_utils/tests_converters/test_unix_timestamp_converter.py
@@ -34,3 +34,10 @@ def test_to_datetime_string(datetime_strings):
         UnixTimestampConverter.to_datetime_str(datetime_strings.expected_ns)
         == datetime_strings.expected_datestring
     )
+
+
+def test_to_th2_timestamp(datetime_strings):
+    assert (
+        UnixTimestampConverter.to_th2_timestamp(datetime_strings.expected_ns)
+        == datetime_strings.expected_th2_timestamp
+    )
