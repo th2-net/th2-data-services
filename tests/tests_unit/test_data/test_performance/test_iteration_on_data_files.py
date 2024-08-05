d07139b2656f09accfad0de9bd00957a33c5719d
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2024-08-05 14:33:07 +0400

    Update __add__ logic

---
+++ tests/tests_unit/test_data/test_performance/test_iteration_on_data_files.py
@@ -0,0 +1,28 @@
+import os
+import time
+import random
+
+from th2_data_services.data import Data
+
+
+def test_iteration_speed():
+    n = 10_000
+    data = Data([random.randint(1, 100_000) for _ in range(n)])
+    directory = "tests/tests_unit/test_data/test_performance/benchmark"
+    os.makedirs(directory, exist_ok=True)
+    data.to_json_lines(f"{directory}/json0.gz", gzip=True, overwrite=True)
+    for i in range(1, 122):
+        data = Data([random.randint(1, 100_000) for _ in range(n)])
+        data.to_json_lines(f"{directory}/json{i}.gz", gzip=True, overwrite=True)
+
+    data = Data.from_json(f"{directory}/json0.gz", gzip=True)
+    for i in range(1, 122):
+        data = data + Data.from_json(f"{directory}/json{i}.gz", gzip=True)
+
+    start_time = time.time()
+    j = 0
+    for _ in data:
+        j += 1
+
+    print(f"Number of records: {j}")
+    print(f"Time took: {time.time() - start_time}")
